731adb669abec7afaf914b14890e3b8f52a182e3
Elvolver <elvolver@mail.ru>
2024-04-19 13:23:31 +0300

    add amigoscode_java_spring_microservices module

---
+++ amigoscode_java_spring_microservices/src/main/java/org/example/amigoscode_java_spring_microservices/AmigoscodeJavaSpringMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package org.example.amigoscode_java_spring_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodeJavaSpringMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigoscodeJavaSpringMicroservicesApplication.class, args);
+    }
+
+}
---
+++ amigoscode_java_spring_microservices/src/test/java/org/example/amigoscode_java_spring_microservices/AmigoscodeJavaSpringMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.amigoscode_java_spring_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodeJavaSpringMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
